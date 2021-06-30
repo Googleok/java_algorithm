60092c5360b6a515a2d9a4ee0e944b5b50bc6e93
Googleok <qkrwhddjr3@gmail.com>
2021-06-30 09:34:53 +0900

    완주하지 못한 선수 문제 해결

---
+++ src/programmers/level1/hash-player_who_did_not_completed.java
@@ -0,0 +1,40 @@
+package programmers.level1;
+
+import java.util.*;
+
+class Solution001 {
+    public String sol(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String name : participant) {
+            int nameCount = 1;
+            if (map.containsKey(name))
+                nameCount += map.get(name);
+            map.put(name, nameCount);
+        }
+
+        for (String name : completion) {
+            int nameCount = map.get(name);
+            map.put(name, nameCount - 1);
+        }
+
+        return getKey(map, 1);
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for (K key : map.keySet()) {
+            if (value.equals(map.get(key))) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] participant = { "leo", "kiki", "eden" };
+        String[] completion = { "eden", "kiki" };
+
+        Solution001 solution = new Solution001();
+        String answer = solution.sol(participant, completion);
+        System.out.println(answer);
+    }
+}
