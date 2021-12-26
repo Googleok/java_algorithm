5535c169cd40aceea35d9c22ceee3c663a8f642c
Googleok <qkrwhddjr3@gmail.com>
2021-12-26 15:27:11 +0900

    leetcode 2일차

---
+++ src/leetcode/easy/palindrome_number.java
@@ -0,0 +1,19 @@
+package leetcode.easy;
+
+public class palindrome_number {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(isPalindrome(x));
+    }
+
+    static public boolean isPalindrome(int x) {
+        String strX = x + "";
+        String rStrX = "";
+
+        for (int i = strX.length() - 1; i >= 0; i--) {
+            rStrX += strX.charAt(i) + "";
+        }
+
+        return strX.equals(rStrX);
+    }
+}
---
+++ src/leetcode/easy/two_sum.java
@@ -0,0 +1,29 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class two_sum {
+    public static void main(String[] args) {
+        int[] nums = {3, 3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    static public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                answer[0] = map.get(target - nums[i]);
+                answer[1] = i;
+                return answer;
+            }
+            map.put(nums[i], i);
+        }
+
+        return answer;
+    }
+}
